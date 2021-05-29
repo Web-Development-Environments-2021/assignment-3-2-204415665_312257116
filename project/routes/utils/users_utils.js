b0f2776182db8124beb3e967d9df0dc2d6547732
MosheBiran <73993888+MosheBiran@users.noreply.github.com>
2021-05-30 00:22:56 +0300

    api + SQL_searchByQuery

--- project/routes/utils/users_utils.js
+++ project/routes/utils/users_utils.js
@@ -1,5 +1,140 @@
 //init
 const DButils = require("./DButils");
+const axios = require("axios");
+const api_domain = "https://soccer.sportmonks.com/api/v2.0";
+
+
+//------------------------------------------------------------------------------------ //
+// -----------------------------   SQL_searchByQuery   ------------------------------- //
+//------------------------------------------------------------------------------------ //
+
+function SQL_searchByQuery(Search_Query, Search_Type, Sort_Teams_Alphabetica, Sort_Players, Sort_Players_By, Filter_Players) {
+    const Qsearch = getQueryInfo(Search_Query, Search_Type);
+    
+    return Qsearch;
+}
+exports.SQL_searchByQuery = SQL_searchByQuery;
+
+
+// //* ------------------------------ getMatchsInfo ------------------------------ *//
+
+async function getQueryInfo(Search_Query, Search_Type) {
+  let promises = [];
+  var include_params;
+  if (Search_Type == "Teams"){
+    include_params = "league";
+  }
+  else{
+    include_params = `Team , Team.league`;
+  }
+  promises.push(
+      axios.get(`${api_domain}/${Search_Type}/search/${Search_Query}`, {
+        params: {
+          api_token: process.env.api_token,
+
+          include: include_params,
+        },
+      })
+    ) 
+  let matchs_info = await Promise.all(promises);
+  return extractRelevantMatchsData(matchs_info);
+}
+
+//* ---------------------------- extractRelevantPlayerData ---------------------------- *//
+
+function extractRelevantMatchsData(matchs_info) {
+  return matchs_info.map((curr_match_info) => {
+    var homeTeamName = curr_match_info.data.data.localTeam.data["name"];
+    var awayTeamName = curr_match_info.data.data.visitorTeam.data["name"];
+    const { date_time } = curr_match_info.data.data.time.starting_at;
+    var stadium = curr_match_info.data.data.venue.data["name"];
+    return {
+      matchDate: date_time,
+      loaclTeamName: homeTeamName,
+      visitorTeamName: awayTeamName,
+      venueName: stadium,
+    };
+  });
+}
+
+exports.getMatchsInfo = getMatchsInfo;
+
+
+
+
+// ----------------------------------------------------------------------------------- //
+// -----------------------------------   getters   ----------------------------------- //
+// ----------------------------------------------------------------------------------- //
+
+//* ------------------------------ getFavoritePlayers ------------------------------ *//
+
+async function getFavoritePlayers(user_id) {
+  const player_ids = await DButils.execQuery(
+    `select player_id from FavoritePlayers where user_id='${user_id}'`
+  );
+  return player_ids;
+}
+exports.getFavoritePlayers = getFavoritePlayers;
+
+// --------------------   get  user Favorites Teams   ---------------------------- //
+
+async function getFavoriteMatches(user_id) {
+  const match_ids = await DButils.execQuery(
+    `select match_id from FavoriteMatches where user_id='${user_id}'`
+  );
+  return match_ids;
+}
+exports.getFavoriteMatches = getFavoriteMatches;
+
+// --------------------   get User Favorites Teams   ---------------------------- //
+
+async function getFavoriteTeams(user_id) {
+  const team_ids = await DButils.execQuery(
+    `select team_id from FavoriteTeams where user_id='${user_id}'`
+  );
+  return team_ids;
+}
+exports.getFavoriteTeams = getFavoriteTeams;
+
+
+
+
+
+
+
+//------------------------------------------------------------------------------------ //
+// -------------------------------   remove function   ------------------------------- //
+//------------------------------------------------------------------------------------ //
+
+
+// --------------------   remove from Favorites Players    ------------------------ //
+// remove a Players from User Favorites Teams
+async function removePlayersFromFavorite(user_id, player_id) {
+  await DButils.execQuery(
+    `DELETE  FROM  FavoritePlayers WHERE user_id='${user_id}' AND player_id='${player_id}'`
+  );
+}
+exports.removePlayersFromFavorite = removePlayersFromFavorite;
+
+// --------------------   remove from Favorites Matches    ------------------------ //
+// remove a Matches from User Favorites Matches
+async function removeMatchesFromFavorite(user_id, match_id) {
+  await DButils.execQuery(
+    `DELETE  FROM  FavoriteMatches WHERE user_id='${user_id}' AND team_id='${match_id}'`
+  );
+}
+exports.removeMatchesFromFavorite = removeMatchesFromFavorite;
+
+// --------------------   remove from Favorites TeamS    --------------------------- //
+// remove a Team from User Favorites Teams
+async function removeTeamsFromFavorite(user_id, team_id) {
+  await DButils.execQuery(
+    `DELETE  FROM  FavoriteTeams WHERE user_id='${user_id}' AND team_id='${team_id}'`
+  );
+}
+exports.removeTeamsFromFavorite = removeTeamsFromFavorite;
+
+
 
 
 
@@ -39,74 +174,3 @@ async function markTeamsAsFavorite(user_id, team_id) {
 }
 exports.markTeamsAsFavorite = markTeamsAsFavorite;
 
-
-// ----------------------------------------------------------------------------------- //
-// -----------------------------------   getters   ----------------------------------- //
-// ----------------------------------------------------------------------------------- //
-
-
-// --------------------   get user Favorites Players   ----------------------------
-
-
-//* ------------------------------ getFavoritePlayers ------------------------------ *//
-
-async function getFavoritePlayers(user_id) {
-  const player_ids = await DButils.execQuery(
-    `select player_id from FavoritePlayers where user_id='${user_id}'`
-  );
-  return player_ids;
-}
-exports.getFavoritePlayers = getFavoritePlayers;
-
-// --------------------   get  user Favorites Teams   ---------------------------- //
-
-async function getFavoriteMatches(user_id) {
-  const match_ids = await DButils.execQuery(
-    `select match_id from FavoriteMatches where user_id='${user_id}'`
-  );
-  return match_ids;
-}
-exports.getFavoriteMatches = getFavoriteMatches;
-
-// --------------------   get User Favorites Teams   ---------------------------- //
-
-async function getFavoriteTeams(user_id) {
-  const team_ids = await DButils.execQuery(
-    `select team_id from FavoriteTeams where user_id='${user_id}'`
-  );
-  return team_ids;
-}
-exports.getFavoriteTeams = getFavoriteTeams;
-
-
-//------------------------------------------------------------------------------------ //
-// -------------------------------   remove function   ------------------------------- //
-//------------------------------------------------------------------------------------ //
-
-
-// --------------------   remove from Favorites Players    ------------------------ //
-// remove a Players from User Favorites Teams
-async function removePlayersFromFavorite(user_id, player_id) {
-  await DButils.execQuery(
-    `DELETE  FROM  FavoritePlayers WHERE user_id='${user_id}' AND player_id='${player_id}'`
-  );
-}
-exports.removePlayersFromFavorite = removePlayersFromFavorite;
-
-// --------------------   remove from Favorites Matches    ------------------------ //
-// remove a Matches from User Favorites Matches
-async function removeMatchesFromFavorite(user_id, match_id) {
-  await DButils.execQuery(
-    `DELETE  FROM  FavoriteMatches WHERE user_id='${user_id}' AND team_id='${match_id}'`
-  );
-}
-exports.removeMatchesFromFavorite = removeMatchesFromFavorite;
-
-// --------------------   remove from Favorites TeamS    --------------------------- //
-// remove a Team from User Favorites Teams
-async function removeTeamsFromFavorite(user_id, team_id) {
-  await DButils.execQuery(
-    `DELETE  FROM  FavoriteTeams WHERE user_id='${user_id}' AND team_id='${team_id}'`
-  );
-}
-exports.removeTeamsFromFavorite = removeTeamsFromFavorite;
