b565bd11d786d32b76d2977f8435a4dde385fe5d
DanielDinshtein <danieldinsh@gmail.com>
2021-05-29 14:41:25 +0300

    UnionAgent_Utils - add new match

--- project/routes/utils/unionAgent_utils.js
+++ project/routes/utils/unionAgent_utils.js
@@ -1,2 +1,12 @@
 const DButils = require("./DButils");
 
+
+
+//* ------------------------------ Add New Match ------------------------------ *//
+
+async function addNewMatch(matchDate, loaclTeamName, visitorTeamName, venueName) {
+    await DButils.execQuery(
+      `insert into FeatureMatches values ('${matchDate}',${loaclTeamName},${visitorTeamName},${venueName})`
+    );
+  }
+  exports.addNewMatch = addNewMatch;
