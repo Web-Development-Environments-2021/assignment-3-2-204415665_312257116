d3635312a2c7a8452910c90592bf0ff4a278ab84
DanielDinshtein <danieldinsh@gmail.com>
2021-05-30 22:36:09 +0300

    Need The UnionUtils

--- project/routes/utils/matches_utils.js
+++ project/routes/utils/matches_utils.js
@@ -43,41 +43,6 @@ function extractRelevantMatchsData(matchs_info) {
 
 
   
-//* ------------------------------ Add New Future Match ------------------------------ *//
-
-async function addNewFutureMatch(matchDate, localTeamName, visitorTeamName, venueName, refereeID) {
-  
-  if (refereeID == undefined){
-    await DButils.execQuery(
-      `insert into FutureMatches values ('${matchDate}','${localTeamName}','${visitorTeamName}','${venueName}', null)`
-    );
-
-  } else {
-    await DButils.execQuery(
-      `insert into FutureMatches values ('${matchDate}','${localTeamName}','${visitorTeamName}','${venueName}', '${refereeID}')`
-    );
-  }
-}
-exports.addNewFutureMatch = addNewFutureMatch;
-
-
-//* ------------------------------ Add New Past Match ------------------------------ *//
-
-async function addNewPastMatch(matchDate, localTeamName, visitorTeamName, venueName, refereeID) {
-  
-  if (refereeID == undefined){
-    await DButils.execQuery(
-      `insert into PastMatches values ('${matchDate}','${localTeamName}','${visitorTeamName}','${venueName}', null, null, null, null)`
-    );
-
-  } else {
-    await DButils.execQuery(
-      `insert into PastMatches values ('${matchDate}','${localTeamName}','${visitorTeamName}','${venueName}', '${refereeID}', null, null, null)`
-    );
-  }
-}
-exports.addNewPastMatch = addNewPastMatch;
-
 
 //* ------------------------------ Get League Matches ------------------------------ *//
 
@@ -180,41 +145,6 @@ async function extractEventLog(EventID){
 exports.extractEventLog = extractEventLog;
 
 
-//* ------------------------------ Add Past Match Result------------------------------ *//
-
-async function addFutureMatchResult(matchID, matchDate, localTeamName, visitorTeamName, venueName, refereeID, localTeamScore, visitorTeamScore) {
-  
-  if (refereeID == undefined){
-    await DButils.execQuery(
-      `insert into PastMatches values ('${matchDate}','${localTeamName}','${visitorTeamName}','${venueName}', null,'${localTeamScore}','${visitorTeamScore}', null )`
-    );
-
-  } else {
-    await DButils.execQuery(
-      `insert into PastMatches values ('${matchDate}','${localTeamName}','${visitorTeamName}','${venueName}', '${refereeID}','${localTeamScore}','${visitorTeamScore}', null )`
-    );
-  }
-
-  await DButils.execQuery(
-    `delete from FutureMatches where match_id='${matchID}'`
-  );
-
-}
-exports.addFutureMatchResult = addFutureMatchResult;
-
-
-//* ------------------------------ Add Future Match Result------------------------------ *//
-
-async function addPastMatchResult(matchID, localTeamScore, visitorTeamScore) {
-  
-  await DButils.execQuery(
-    `update PastMatches 
-      set localTeamScore='${localTeamScore}', visitorTeamScore='${visitorTeamScore}' 
-      where match_id='${matchID}'`
-  );
-
-}
-exports.addPastMatchResult = addPastMatchResult;
 
 
 
--- project/routes/utils/unionAgent_utils.js
+++ project/routes/utils/unionAgent_utils.js
@@ -1,4 +1,77 @@
 const DButils = require("./DButils");
 
 
+//TODO: Need Make Authentication
 
+//* ------------------------------ Add New Future Match ------------------------------ *//
+
+async function addNewFutureMatch(matchDate, localTeamName, visitorTeamName, venueName, refereeID) {
+  
+  if (refereeID == undefined){
+    await DButils.execQuery(
+      `insert into FutureMatches values ('${matchDate}','${localTeamName}','${visitorTeamName}','${venueName}', null)`
+    );
+
+  } else {
+    await DButils.execQuery(
+      `insert into FutureMatches values ('${matchDate}','${localTeamName}','${visitorTeamName}','${venueName}', '${refereeID}')`
+    );
+  }
+}
+exports.addNewFutureMatch = addNewFutureMatch;
+
+
+//* ------------------------------ Add New Past Match ------------------------------ *//
+
+async function addNewPastMatch(matchDate, localTeamName, visitorTeamName, venueName, refereeID) {
+  
+  if (refereeID == undefined){
+    await DButils.execQuery(
+      `insert into PastMatches values ('${matchDate}','${localTeamName}','${visitorTeamName}','${venueName}', null, null, null, null)`
+    );
+
+  } else {
+    await DButils.execQuery(
+      `insert into PastMatches values ('${matchDate}','${localTeamName}','${visitorTeamName}','${venueName}', '${refereeID}', null, null, null)`
+    );
+  }
+}
+exports.addNewPastMatch = addNewPastMatch;
+
+
+
+//* ------------------------------ Add Past Match Result------------------------------ *//
+
+async function addFutureMatchResult(matchID, matchDate, localTeamName, visitorTeamName, venueName, refereeID, localTeamScore, visitorTeamScore) {
+  
+  if (refereeID == undefined){
+    await DButils.execQuery(
+      `insert into PastMatches values ('${matchDate}','${localTeamName}','${visitorTeamName}','${venueName}', null,'${localTeamScore}','${visitorTeamScore}', null )`
+    );
+
+  } else {
+    await DButils.execQuery(
+      `insert into PastMatches values ('${matchDate}','${localTeamName}','${visitorTeamName}','${venueName}', '${refereeID}','${localTeamScore}','${visitorTeamScore}', null )`
+    );
+  }
+
+  await DButils.execQuery(
+    `delete from FutureMatches where match_id='${matchID}'`
+  );
+
+}
+exports.addFutureMatchResult = addFutureMatchResult;
+
+
+//* ------------------------------ Add Future Match Result------------------------------ *//
+
+async function addPastMatchResult(matchID, localTeamScore, visitorTeamScore) {
+  
+  await DButils.execQuery(
+    `update PastMatches 
+      set localTeamScore='${localTeamScore}', visitorTeamScore='${visitorTeamScore}' 
+      where match_id='${matchID}'`
+  );
+
+}
+exports.addPastMatchResult = addPastMatchResult;
